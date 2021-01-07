569fca0b4cb6af963a922346745965ce8bee0b48
hodongman <jhd9206@gmail.com>
2021-01-08 03:38:17 +0900

    regist page update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { LoginComponent } from './components/login/login' 
+import { LoginComponent } from './components/login' 
+import { UserRegistComponent } from './components/reigster'
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -12,6 +13,7 @@ function App() {
         <div>
             <Switch>
                 <Route exact={true} path="/" component={LoginComponent} />
+                <Route path="/regist" component={UserRegistComponent} />
             </Switch>
         </div>
     </Router>
